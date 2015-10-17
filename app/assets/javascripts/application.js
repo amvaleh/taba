78d7a0599df2be678972ce2f56c2548a2ec29052
amvaleh <amvaleh@gmail.com>
2015-10-17 13:47:50 +0330

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,38 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require ga
+//= require jquery.themepunch.plugins.min
+//= require jquery.themepunch.revolution.min
+//= require jquery.form.min
+//= require scripts
+//= require comment-reply.min
+//= require modernizr.custom
+//= require jquery.easing.1.3
+//= require jquery.appear
+//= require afterresize
+//= require imagesloaded.pkgd.min
+//= require jquery.carouFredSel-6.2.1
+//= require jquery.touchSwipe.min
+//= require jquery.flexslider
+//= require jquery.fitvids
+//= require retina.min
+//= require jquery.hoverdir
+//= require jquery.isotope.min
+//= require jquery.magnific-popup.min
+//= require jquery.parallax.min
+//= require jquery.transit
+//= require jquery.sticky
+//= require main
---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+
+  layout 'application'
+  def home
+  	
+  end
+end
